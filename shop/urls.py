945aadba3eeeb056d437799084f4e3991f41093a
Mohit Devda <mohit2797@gmail.com>
2020-10-07 23:18:43 +0530

    Added base.html and home.html and made required changes in settings.py, views.py and urls.py

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from . import views
+from .views import *
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    # path('', indexView.as_view(), name='home'),
+    path('', homeView, name='home'),
+]
