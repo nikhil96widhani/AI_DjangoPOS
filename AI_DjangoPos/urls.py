241848ca391c6bf9371e30e236162a7f61a7f715
Mohit Devda <mohit2797@gmail.com>
2020-10-08 18:31:27 +0530

    Fixed urls.py

--- AI_DjangoPos/urls.py
+++ AI_DjangoPos/urls.py
@@ -21,6 +21,6 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
+    path('', include('inventory.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
