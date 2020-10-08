85ac013199e38c1f3223a971953334536ed20f1c
Nikhil Widhani <46464842+nikhil96widhani@users.noreply.github.com>
2020-10-08 14:16:23 -0400

    added products

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from .models import *
 
 
+class ProductList(admin.ModelAdmin):
+    list_display = ('name', 'product_code', 'quantity')
+    # exclude = ('amount',)
+
+
 class OrderItemList(admin.ModelAdmin):
     list_display = ('product', 'order', 'quantity', 'amount')
     exclude = ('amount',)
 
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductList)
 admin.site.register(ProductCategories)
 admin.site.register(Order)
 admin.site.register(OrderItem, OrderItemList)
