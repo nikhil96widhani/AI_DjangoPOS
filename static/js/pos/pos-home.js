e8044987968a8034dcbeabdbe0d19230a36cb34c
Nikhil Widhani <46464842+nikhil96widhani@users.noreply.github.com>
2020-10-09 16:49:58 -0400

    fixes UI

--- static/js/pos/pos-home.js
+++ static/js/pos/pos-home.js
@@ -13,3 +13,18 @@ setTimeout(function () {
 $('#horizontal-stepper').nextStep();
 }, 2000);
 }
+
+// Refund Calculator
+function CalculateRefund(cash, total) {
+    if (cash<total){
+        document.getElementById('RefundAmount').innerHTML = "Less Cash Recieved";
+    }
+    else if (cash>total){
+        document.getElementById('RefundAmount').innerHTML = "Refund: " + (cash - total).toString();
+    }
+    else {
+        document.getElementById('RefundAmount').innerHTML = "No items to calculate or unknown error";
+    }
+
+  }
+
