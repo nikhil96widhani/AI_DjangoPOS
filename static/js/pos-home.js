f44978efbc3e1f035d48691d9ebc812bb985cba3
Nikhil Widhani <46464842+nikhil96widhani@users.noreply.github.com>
2020-10-11 19:20:20 -0400

    Login added with homepage router

--- static/js/pos-home.js
+++ static/js/pos-home.js
@@ -85,52 +85,7 @@ function loadTable() {
                 '<span id="cart-total-amount">' +data.cart_total+ '</span>')
             var trHTML = '';
             $.each(data.items, function (e, item) {
-                trHTML += '<tr>' +
-                    '    <td>' +
-                    '        <div class="font-weight-bold">' +
-                    item.product.name +
-
-                    '        </div>' +
-                    '    </td>' +
-                    '    <td>' +
-                    item.product.weight +
-                    '    </td>' +
-                    '    <td>₹' + item.product.discount_price + '</td>' +
-                    '    <td class="text-center text-md-center">' +
-                    '        <span class="qty">' + item.quantity + ' </span>' +
-
-                    '<div class="btn-group radio-group ml-2" data-toggle="buttons">' +
-                    '                                <button type="button"' +
-                    '                                        data-product="1001"' +
-                    '                                        data-action="remove"' +
-                    '                                        class="btn btn-primary btn-sm mr-1 btn-rounded" ' +
-                    'onclick="updateUserOrder(' + item.product.product_code + ', \'remove\')"><i' +
-                    '                                        class="fas fa-minus"></i>' +
-                    '                                </button>' +
-                    '                                <button type="button"' +
-                    '                                        data-product="1001"ß' +
-                    '                                        data-action="add"' +
-                    '                                        class="btn btn-primary btn-sm mr-1 btn-rounded" ' +
-                    'onclick="updateUserOrder(' + item.product.product_code + ', \'add\')"><i' +
-                    '                                        class="fas fa-plus"></i>' +
-                    '                                </button>' +
-                    '                            </div>' +
-
-                    '    </td>' +
-                    '    <td class="font-weight-bold">' +
-                    '        <strong>₹' + item.amount + '</strong>' +
-                    '    </td>' +
-                    '    <td>' +
-                    '        <button type="button"' +
-                    '                                        data-product="1001"' +
-                    '                                        data-action="add"' +
-                    '                                        class="btn btn-danger btn-sm mr-1 btn-rounded" ' +
-                    'onclick="updateUserOrder(' + item.product.product_code + ', \'delete\')"><i' +
-                    '                                        class="fas fa-trash"></i>' +
-                    '                                </button>' +
-                    '        </button>' +
-                    '    </td>' +
-                    '</tr>';
+                trHTML += `<tr><td><div class="font-weight-bold">${item.product.name}        </div>    </td>    <td>${item.product.weight}    </td>    <td>₹${item.product.discount_price}</td>    <td class="text-center text-md-center">        <span class="qty">${item.quantity} </span><div class="btn-group radio-group ml-2" data-toggle="buttons">                                <button type="button"                                        data-product="1001"                                        data-action="remove"                                        class="btn btn-primary btn-sm mr-1 btn-rounded" onclick="updateUserOrder(${item.product.product_code}, 'remove')"><i                                        class="fas fa-minus"></i>                                </button>                                <button type="button"                                        data-product="1001"ß                                        data-action="add"                                        class="btn btn-primary btn-sm mr-1 btn-rounded" onclick="updateUserOrder(${item.product.product_code}, 'add')"><i                                        class="fas fa-plus"></i>                                </button>                            </div>    </td>    <td class="font-weight-bold">        <strong>₹${item.amount}</strong>    </td>    <td>        <button type="button"                                        data-product="1001"                                        data-action="add"                                        class="btn btn-danger btn-sm mr-1 btn-rounded" onclick="updateUserOrder(${item.product.product_code}, 'delete')"><i                                        class="fas fa-trash"></i>                                </button>        </button>    </td></tr>`;
 
             });
             $('#datatable-ajax').empty().append(trHTML);
