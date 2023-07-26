61ccdd1fcab69c3e34c8597123e1828da385eec6
nikhil96widhani <nikhil96widhani@gmail.com>
2023-07-25 23:13:17 -0400

    Latest

--- app/AI_DjangoPos/settings.py
+++ app/AI_DjangoPos/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'AI_DjangoPos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'db/db.sqlite3',
     }
 }
 
--- app/runapp.sh
+++ app/runapp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # cd cfg
 
-echo "[LOADING] ENV variables"
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
+#echo "[LOADING] ENV variables"
+#if [ -f .env ]
+#then
+#  export $(cat .env | sed 's/#.*//g' | xargs)
+#fi
 
 echo "[RUNNING] migrations"
 python manage.py makemigrations
@@ -14,10 +14,12 @@ python manage.py migrate --no-input
 echo "[COPYING] Collecting Static Files"
 python manage.py collectstatic --noinput
 
-echo "[RUNNING] app on port $WEBAPP_PORT"
-if [ $WEBAPP_DEBUG_MODE == "True" ]
-then
-  python manage.py runserver 0.0.0.0:8000
-else
-  gunicorn webapp.webapp.wsgi -b 0.0.0.0:8000 --log-level debug
-fi
+echo "[RUNNING] app on port 8000"
+python manage.py runserver 0.0.0.0:8000
+#echo "[RUNNING] app on port $WEBAPP_PORT"
+#if [ $WEBAPP_DEBUG_MODE == "True" ]
+#then
+#  python manage.py runserver 0.0.0.0:8000
+#else
+#  gunicorn webapp.webapp.wsgi -b 0.0.0.0:8000 --log-level debug
+#fi
